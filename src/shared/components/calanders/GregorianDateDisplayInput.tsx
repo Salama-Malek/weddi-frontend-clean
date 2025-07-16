fa1adc13626ceddf57ba0037c4ecba0109db0fea
Salama-Malek <salamahassanein@gmail.com>
2025-07-16 10:28:02 +0300

    aligned icon of reg date to right in arabic system

--- src/shared/components/calanders/GregorianDateDisplayInput.tsx
+++ src/shared/components/calanders/GregorianDateDisplayInput.tsx
@@ -3,6 +3,7 @@ import { Control, Controller } from "react-hook-form";
 import { FieldWrapper } from "@/shared/components/form";
 import { FormData } from "@/shared/components/form/form.types";
 import { Calculator01Icon } from "hugeicons-react";
+import { useTranslation } from "react-i18next";
 
 interface GregorianDateDisplayInputProps {
   control: Control<any>;
@@ -21,6 +22,7 @@ export const GregorianDateDisplayInput: React.FC<GregorianDateDisplayInputProps>
   isError,
   notRequired,
 }) => {
+  const { i18n } = useTranslation();
   const formatDateForDisplay = (date: string) => {
     if (!date || date.length !== 8) return date;
     return `${date.slice(0, 4)}/${date.slice(4, 6)}/${date.slice(6)}`;
@@ -39,7 +41,7 @@ export const GregorianDateDisplayInput: React.FC<GregorianDateDisplayInputProps>
               value={formatDateForDisplay(value || "")}
               readOnly
             />
-            <Calculator01Icon className="absolute right-3 top-1/2 -translate-y-1/2 text-secondary-400" />
+            <Calculator01Icon className={`absolute ${i18n.dir() === "rtl" ? "left-3" : "right-3"} top-1/2 -translate-y-1/2 text-secondary-400`} />
             {(isError) && (
               <div className="invalid-feedback text-red-500 mt-2">
                 {invalidFeedback}
