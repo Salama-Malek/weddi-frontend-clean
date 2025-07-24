be29bbd90037e776c5a75438eef9146c55027eed
Salama Malek <116031625+Salama-Malek@users.noreply.github.com>
2025-07-24 14:22:01 +0000

    chore: remove unused files

--- build.ts
+++
@@ -1,3 +0,0 @@
-// start-base/15-7 is the base branch for the latest build sent on 19-7 for QC only
-// build/19-7-25 is the base branch for the latest build sent on 19-7 for QC - preprod - prod
-// build/21-7-25 is the base branch for the latest build sent on 21-7 for QC - preprod - prod
